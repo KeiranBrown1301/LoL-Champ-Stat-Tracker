264e785ca7d9147c0bbf659e0d3d91d290a74abd
Keiran Brown <keiranbrown1301@gmail.com>
2022-06-17 17:24:05 +0900

    tried to call the API, but need to work with the data more.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      DataisLoaded: false
-    };
-  }
-  componentDidMount() {
-    fetch(
-      "http://ddragon.leagueoflegends.com/cdn/12.11.1/data/en_US/champion.json")
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({
-          items: json,
-          DataisLoaded: true
-        })
-      })
-  }
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [champs, setChamps] = useState([]);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>LoL Champ Stat Tracker</h1>
-      </header>
-      <div>
-        <p>
-          <i>
-            Lol Champ Stat Tracker isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.
-          </i>
-        </p>
+  useEffect(() => {
+    fetch("http://ddragon.leagueoflegends.com/cdn/12.11.1/data/en_US/champion.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setChamps(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>LoL Champ Stat Tracker</h1>
+        </header>
+        <div>
+          <p>
+            <i>
+              Lol Champ Stat Tracker isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.
+            </i>
+          </p>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
